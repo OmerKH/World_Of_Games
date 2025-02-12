cd0c9b031bbaa933290b6c73b9bf2e40ff1416b2
Omer Kirshner <omer.khs@gmail.com>
2025-02-12 11:01:28 +0200

    orginized code in folders

---
+++ scores/main_score.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from utils import BAD_RETURN_CODE
+
+# display in HTML
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def score_server():
+    score = "0"
+    try:
+        with open("Scores.txt", "r", encoding="utf-8") as file:
+            score = file.read().strip()
+            if not score.isdigit():
+                return f"""
+                    <html>
+                        <head>
+                            <title>Score Game</title>
+                        </head>
+                        <body>
+                            <h1>Error</h1>
+                            <div id="score" style="color:red;">{BAD_RETURN_CODE}</div>
+                        </body>
+                    </html>
+                """
+            else:
+                return f"""
+                    <html>
+                        <head>
+                            <title>Score Game</title>
+                        </head>
+                        <body>
+                            <h1>The score is:</h1>
+                            <div id="score">{score}</div>
+                        </body>
+                    </html>
+                """
+    except FileNotFoundError:
+        return f"""
+        <html>
+            <head>
+                <title>Score Game</title>
+            </head>
+            <body>
+                <h1>Error</h1>
+                <div id="score" style="color:red;">{BAD_RETURN_CODE}</div>
+            </body>
+        </html>
+        """
